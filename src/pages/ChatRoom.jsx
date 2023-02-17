e62cdbc62106859ed504e75fca9d4fa2fc25ab16
Hisham <hisham0502@gmail.com>
2023-02-17 22:20:44 +0530

    README.md file updated and ChatRoom.jsx

--- src/pages/ChatRoom.jsx
+++ src/pages/ChatRoom.jsx
@@ -152,7 +152,7 @@ export default function ChatRoom() {
             justifyContent: "space-between",
           }}
         >
-          {/* <h4 style={{ margin: 0 }}>{user.displayName} </h4> */}
+          <h4 style={{ margin: 0 }}>All Users</h4>
         </div>
         <div style={{ overflowY: "scroll" }}>
           <UsersComponent
